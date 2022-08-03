1f7b42703215de969cbbc567d9c9700a77b7bb00
CarlosD95 <carlosdaserrano4@gmail.com>
2022-08-03 18:15:39 -0400

    Codigo Nuevo

---
+++ src/componentes/formulario.js
@@ -0,0 +1,46 @@
+import "../hojas-de-estilo/formulario.css"
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid'
+
+function TareaFormulario (props) {
+
+    const [input , setInput] = useState('');
+
+    const manejarCambio = e => {
+        setInput(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const manejarEnvio = e => {
+        e.preventDefault();
+        console.log("enviando");
+
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        }
+
+        props.onSubmit(tareaNueva);
+
+    };
+
+    return(
+        <form className="tarea-formulario"
+            onSubmit={manejarEnvio}
+        >
+            <input 
+                className="tarea-input"
+                type='text'
+                placeholder="Escribe una tarea por realizar"
+                name="texto"
+                onChange={manejarCambio}
+            />
+            <button className="tarea-boton">
+                Agregar Tarea
+            </button>
+        </form>
+    );
+}
+
+export default TareaFormulario;
